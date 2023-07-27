bb04161c1dd367d3d3363644afe95cd0ea48f616
Zino <72712106+ZinoChan@users.noreply.github.com>
2023-07-27 14:27:02 +0100

    refactor projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -1,15 +1,15 @@
 export const projects = [
-  {
-    id: "5ff76e76-4fc3-4ba4-8652-1866a8e734zz",
-    category: "MERN Stack",
-    name: "Lapino",
-    img: "/lp-home.png",
-    linkToPage: "https://lapino.onrender.com/",
-    linkToSrc: "https://github.com/ZinoChan/lapino",
-    description: "E-commerce for Kids products",
-    tools: ["React", "Express", "Tailwindcss", "Redux", "Firebase-admin", "Typescript"],
-    imgs: ["/lp-home.png", "/work/lp-details.png", "/work/lp-login.png", "/work/lp-cart.png"]
-  },
+   {
+      id: "9f4d390d-1291-4523-8875-3422c0d37ba3",
+      category: "React",
+      name: "Run",
+      img: "/run.jpg",
+      linkToPage: "https://run-store.vercel.app/",
+      linkToSrc: "https://github.com/ZinoChan/Run",
+      description: "sneakers ecommerce app",
+      tools: ["React", "Redux", "Redux Saga", "Firebase", "Bootstrap"],
+      imgs: ["/run.jpg","/work/run1.jpg", "/work/run2.jpg",]
+    },
   {
     id: "6a29d826-fbba-4093-8eef-ac3642c79bbf",
     category: "Nextjs",
@@ -21,17 +21,6 @@ export const projects = [
     tools: ["Next js", "Redux Toolkit", "Redux-persist", "Redux Saga", "Firebase", "Tailwindcss"],
     imgs: ["/work/oishi1.jpg", "/work/oishi2.jpg", "/work/oishi3.jpg"]
   },
-    {
-      id: "9f4d390d-1291-4523-8875-3422c0d37ba3",
-      category: "React",
-      name: "Run",
-      img: "/run.jpg",
-      linkToPage: "https://run-store.vercel.app/",
-      linkToSrc: "https://github.com/ZinoChan/Run",
-      description: "sneakers ecommerce app",
-      tools: ["React", "Redux", "Redux Saga", "Firebase", "Bootstrap"],
-      imgs: ["/run.jpg","/work/run1.jpg", "/work/run2.jpg",]
-    },
     {
       id: "4e701cf5-98a2-4f42-95b5-758cc122d08d",
       category: "Nextjs",
